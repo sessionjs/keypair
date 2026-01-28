817829c427e4f5403bd834f89b3bd163f5b11396
Viktor Shchelochkov <hi@hloth.dev>
2026-01-28 01:46:59 +0100

    Fix key generation

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,42 @@
 import { ed25519 } from '@noble/curves/ed25519.js'
 import { bytesToHex, hexToBytes, randomBytes } from '@noble/curves/utils.js'
 
-export type Keypair = {
-  x25519: SodiumKeypair
-  ed25519: SodiumKeypair
+export type SessionKeys = {
+  x25519: KeyPair
+  ed25519: KeyPair
 }
 
-export interface SodiumKeypair {
+export type KeyPair = {
+  keyType: string
   privateKey: Uint8Array
   publicKey: Uint8Array
-  keyType: string
 }
 
-/**
- * Generate Ed25519 keypair from "seed"
- * @param seed Seed value
- * @param includePublicKey Include public key to secret key value (like libsodium)
- */
-const keyPairFromSeed = (seed: Uint8Array, includePublicKey = true): SodiumKeypair => {
-  let pk = ed25519.getPublicKey(seed)
-  let sk = new Uint8Array(includePublicKey ? 64 : 32)
-  sk.set(seed, 0)
-  if (includePublicKey) {
-    sk.set(pk, 32)
-  }
-  
-  return {
-    keyType: "ed25519",
-    privateKey: sk,
-    publicKey: pk
-  }
-}
-
-export function getKeypairFromSeed(seedHex: string): Keypair {
+export function getKeysFromSeed(seedHex: string): SessionKeys {
   const privKeyHexLength = 32 * 2
   if (seedHex.length !== privKeyHexLength) {
     seedHex = seedHex.concat('0'.repeat(32))
     seedHex = seedHex.substring(0, privKeyHexLength)
   }
   const seed = hexToBytes(seedHex)
-  const ed25519KeyPair = keyPairFromSeed(seed)
-  const x25519PublicKey = ed25519.utils.toMontgomery(ed25519KeyPair.publicKey)
+
+  const ed25519KeyPair = {
+    keyType: "ed25519",
+    privateKey: seed,
+    publicKey: ed25519.getPublicKey(seed)
+  }
+
   const x25519SecretKey = ed25519.utils.toMontgomerySecret(ed25519KeyPair.privateKey)
+  const x25519PublicKey = ed25519.utils.toMontgomery(ed25519KeyPair.publicKey)
 
   const prependedX25519PublicKey = new Uint8Array(33)
   prependedX25519PublicKey.set(x25519PublicKey, 1)
   prependedX25519PublicKey[0] = 5
 
-  const x25519KeyPair: SodiumKeypair = {
+  const x25519KeyPair: KeyPair = {
     keyType: 'x25519',
-    publicKey: prependedX25519PublicKey,
     privateKey: x25519SecretKey,
+    publicKey: prependedX25519PublicKey,
   }
 
   return { x25519: x25519KeyPair, ed25519: ed25519KeyPair }
